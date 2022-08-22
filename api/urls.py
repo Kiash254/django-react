2028849cb99fe0982cd537486d9274384895bede
Kiash254 <codewithkiash@gmail.com>
2022-08-22 20:01:44 +0300

    added new app

--- api/urls.py
+++ api/urls.py
@@ -4,7 +4,7 @@ from . import views
 app_name='api'
 
 urlpatterns = [
-    path('',views.Todoview, name='todoview'),
+    path('todoview/',views.Todoview, name='todoview'),
     path('tododetail/<int:pk>/',views.TodoDetail, name='tododetail'),
     path('todocreate/',views.TodoCreate, name='todocreate'),
     path('todoupdate/<int:pk>/',views.TodoUpdate, name='todoupdate'),
